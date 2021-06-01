e9afcb3d3f66981cf4ee033e20ea7bdaf39855fe
Jacob Schneck <jschneck@Gerald.localdomain>
2021-06-01 16:52:35 -0500

    Finished

--- Virtual_Memory_Management/src/lib.rs
+++ Virtual_Memory_Management/src/lib.rs
@@ -9,9 +9,19 @@ pub const NUM_FRAMES: u32 = 128;
 pub const PAGE_SIZE: u32 = 256;
 
 pub struct PageTableInfo {
-    page_table: [u32; NUM_PAGES as usize],
+    page_table: [i32; NUM_PAGES as usize], // -1 means page not in memory
     access_time: [u32; NUM_FRAMES as usize],
-    free_frame: [u32; NUM_FRAMES as usize],
+    free_frame: [bool; NUM_FRAMES as usize],
+}
+
+impl PageTableInfo {
+    pub fn new() -> PageTableInfo {
+        PageTableInfo {
+            page_table: [-1; NUM_PAGES  as usize],
+            access_time: [0; NUM_FRAMES as usize],
+            free_frame: [true; NUM_FRAMES as usize],
+        }
+    }
 }
 
 pub fn decode_address(address: u32, page_number: &mut u32, page_offset: &mut u32) {
@@ -27,6 +37,65 @@ pub fn read_from_backing_store(f: &mut File, buffer: &mut [u8], page_number: u32
     f.seek(SeekFrom::Start( (PAGE_SIZE * page_number).into() ));
     f. read(buffer);
 }
+
+pub fn get_frame_number(info: &mut PageTableInfo, logical_page_number: u32, access_time: u32, page_fault: &mut bool) -> i32 {
+
+    // Find free frame index
+    let mut frame_number: i32 = 0;
+    let mut free_frame_found = false;
+    for i in 0..NUM_FRAMES {
+        if info.free_frame[i as usize] {
+            frame_number = i as i32;
+            free_frame_found = true;
+            break;
+        }
+    }
+
+    // if no free frame find least recently accessed (LRU)
+    if !free_frame_found {
+        let mut min = 0;
+        for i in 0..NUM_FRAMES {
+            if info.access_time[i as usize] < info.access_time[min as usize] {
+                min = i;
+            }
+        }
+        frame_number = min as i32;
+    }
+
+    if info.page_table[logical_page_number as usize] < 0 {
+        if !free_frame_found {
+            let mut p: u32 = 0;
+            for i in 0..NUM_PAGES {
+                if info.page_table[i as usize] == frame_number {
+                    info.page_table[i as usize] = -1;
+                    p = i;
+                    break;
+                }
+            }
+            println!("EVICT! Oldest frame is {}, (age = {})", frame_number, info.access_time[frame_number as usize]);
+            // println!("The page mapped to frame {} is {}: page {} is now unmapped (not in memory)\n", frame_number, p, p);
+
+            *page_fault = true;
+            info.access_time[frame_number as usize] = access_time;
+            info.page_table[logical_page_number as usize] = frame_number;
+            info.free_frame[frame_number as usize] = false;
+        } else {
+            *page_fault = true;
+            info.access_time[frame_number as usize] = access_time;
+            info.page_table[logical_page_number as usize] = frame_number;
+            info.free_frame[frame_number as usize] = false;
+        }
+    } else { // logical page number is in page table
+        *page_fault  = false;
+        frame_number = info.page_table[logical_page_number as usize];
+        info.access_time[frame_number as usize] = access_time;
+        info.free_frame[frame_number as usize] = false;
+    }
+
+    // return in rust syntax :()
+    frame_number
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -100,6 +169,8 @@ mod tests {
 
         read_from_backing_store(&mut f, &mut buffer, 0);
         assert!(buffer[0] == 0 && buffer[255] == 63 && buffer[171] == 42);
+        read_from_backing_store(&mut f, &mut buffer, 5);
+        assert!(buffer[0] == 0 && buffer[255] == 127 && buffer[171] == 106);
     }
 }
     
--- Virtual_Memory_Management/src/main.rs
+++ Virtual_Memory_Management/src/main.rs
@@ -1,3 +1,59 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+use Virtual_Memory_Management::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f_b: File = File::open("BACKING_STORE.dat").unwrap();
+    let mut info: PageTableInfo = PageTableInfo::new();
+
+    let mut access_time = 0;
+    let mut page_number: u32 = 0;
+    let mut page_offset: u32 = 0;
+    let mut page_fault = false;
+    let mut buffer: [u8; PAGE_SIZE as usize] = [0; PAGE_SIZE as usize];
+    // let mut physical_memory;  
+    let mut physical_memory = vec![&[0; PAGE_SIZE as usize]; (PAGE_SIZE*NUM_FRAMES) as usize];
+    
+    if let Ok(lines) = read_lines("./addresses.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                let address: u32 = ip.parse::<u32>().unwrap();
+                decode_address(address, &mut page_number, &mut page_offset);
+                let frame_number = get_frame_number(&mut info, page_number, access_time, &mut page_fault);
+                if page_fault {
+                    read_from_backing_store(&mut f_b, &mut buffer, page_number);
+                    // physical_memory[(frame_number*(PAGE_SIZE as i32)) as usize];
+                    println!("* Virtual Address: {} [{}, {}] Physical Address: {} [{}, {}]",
+                        address,
+                        page_number,
+                        page_offset,
+                        (frame_number*(PAGE_SIZE as i32) + page_offset as i32),
+                        frame_number,
+                        page_offset,
+                        // physical_memory[(frame_number*(PAGE_SIZE as i32) + page_offset as i32) as usize],
+                    );
+                } else {
+                    println!("Virtual Address: {} [{}, {}] Physical Address: {} [{}, {}]",
+                        address,
+                        page_number,
+                        page_offset,
+                        (frame_number*(PAGE_SIZE as i32) + page_offset as i32),
+                        frame_number,
+                        page_offset,
+                        // physical_memory[(frame_number*(PAGE_SIZE as i32) + page_offset as i32) as usize],
+                    );
+                }
+                access_time += 1;
+            }
+        }
+    }
+    println!("{}", access_time);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
